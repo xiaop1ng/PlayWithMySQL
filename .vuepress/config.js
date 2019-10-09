22aefb3bc446ccd53e71b77dcc5698cb7820afab
xiaop1ng <784516419@qq.com>
2019-10-09 15:59:25 +0800

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/xiaop1ng/PlayWithMySQL' },
+    ],
+	sidebar: 'auto'
+  }
+}
