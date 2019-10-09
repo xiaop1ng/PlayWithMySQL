71c50549ca04a87f7f33b0ff23107d1ef2535ce0
xiaop1ng <784516419@qq.com>
2019-10-09 16:09:06 +0800

    vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,6 +4,11 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'GitHub', link: 'https://github.com/xiaop1ng/PlayWithMySQL' },
     ],
-	sidebar: 'auto'
+	sidebar: [
+      ['/', 'Home'],
+	  ['/doc/create-db-playground', '创建实验场'],
+      ['/doc/about-db-index', '索引'],
+      ['/doc/about-db-explain', '执行计划']
+    ]
   }
 }
