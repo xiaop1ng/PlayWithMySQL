e8818d44f091f52cb2a53620eaf32d654c37def4
xiaop1ng <784516419@qq.com>
2019-10-09 16:13:38 +0800

    vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,12 +3,14 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'GitHub', link: 'https://github.com/xiaop1ng/PlayWithMySQL' },
-    ],
+    ], // 导航栏
 	sidebar: [
       ['/', 'Home'],
 	  ['/doc/create-db-playground', '创建实验场'],
       ['/doc/about-db-index', '索引'],
       ['/doc/about-db-explain', '执行计划']
-    ]
+    ], // 侧栏
+	lastUpdated: 'Last Updated', // 最后更新时间
+	
   }
 }
